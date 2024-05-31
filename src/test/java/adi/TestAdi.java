6a1a5c6223ecc99093585a3551a13333ae151b12
Michał Antczak <michalantczak@outlook.com>
2024-05-31 22:49:04 +0200

    v2.0

--- src/test/java/adi/TestAdi.java
+++ src/test/java/adi/TestAdi.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import static adi.enums.ExpectedPageTitles.XTB_PAGE;
 
@@ -21,8 +22,13 @@ public class TestAdi {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().deleteAllCookies();
+
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
     }
